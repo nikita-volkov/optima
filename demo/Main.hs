e48f1222cb0e60a2c5f1450d352e12fa0e157635
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-14 01:28:59 +0300

    Update the demo

--- demo/Main.hs
+++ demo/Main.hs
@@ -15,15 +15,24 @@ main = parseOpts >>= print where
             (Optima.showable True)
             Optima.unformatted
             (Optima.explicitlyParsed Attoparsec.bool)))
-        (Optima.param (Just 'b') "arg-b"
-          (Optima.value
-            "Description of B"
-            Optima.defaultless
-            Optima.unformatted
-            (Optima.explicitlyParsed Attoparsec.text)))
-        (Optima.param Nothing "arg-c"
-          (Optima.value
-            ""
-            Optima.defaultless
-            Optima.unformatted
-            (Optima.explicitlyParsed Attoparsec.utf8Bytes))))
+        (Optima.group "group1"
+          (liftA2 (,)
+            (Optima.member "bb"
+              (Optima.value
+                "Description of B"
+                Optima.defaultless
+                Optima.unformatted
+                (Optima.explicitlyParsed Attoparsec.text)))
+            (Optima.member "d" textParam)))
+        (Optima.group "group-of-alternatives"
+          (asum [
+            Optima.member "e" textParam,
+            Optima.subgroup "subgroup" (asum [
+                Optima.member "f" textParam,
+                Optima.member "g" (flag $> "slkdfjsdkj")
+              ])
+            ])))
+    where
+      textParam = Optima.value "Text param" Optima.defaultless Optima.unformatted (Optima.explicitlyParsed Attoparsec.text)
+      flag = Optima.flag "Flag type 1"
+      bytesParam = Optima.value "" Optima.defaultless Optima.unformatted (Optima.explicitlyParsed Attoparsec.utf8Bytes)
